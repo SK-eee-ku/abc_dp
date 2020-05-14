13d94530e97d99a67ce86672ca17ecbed83b5bce
Sota Kondo <s.kondo11235813@icloud.com>
2020-05-14 21:34:30 +0900

    D うまくいった

--- D_Knapsack1.py
+++ D_Knapsack1.py
@@ -1,15 +1,26 @@
+#  numpyは偉大
 import numpy as np
 
-#  n, w = list(map(int, input().split()))
-#  wv = np.array([list(map(int, input().split())) for i in range(n)])
-n, w = 3, 8
-wv = np.array([[3, 30], [4, 50], [5, 60]])
+#  入力
+n, w = list(map(int, input().split()))
+wv = np.array([list(map(int, input().split(" "))) for _ in range(n)])
 
+#  dpテーブルを0で初期化
+#  行:重さの上限(0からwまでのw+1要素)
+#  列:使っていい荷物の最大添字(例えば3列目はwv[3]までを使った最適値)
+#  i行j列:wv[j]までを使って重さi以下になるようにした時の最大価値
 dp = np.zeros((w + 1, n), dtype=np.int)
-dp[w[0, 0] :, 0] = np.full(dp[w[0, 0] :, 0].shape, w[0, 1])
+
+#  wv[0]だけ別で更新
+dp[wv[0, 0] :, 0] = np.full(dp[wv[0, 0] :, 0].shape, wv[0, 1])
+
+#  dpテーブルの完成
 for i in range(1, n):
+    #  とりあえずwv[i-1]までの最適値をパクる
     dp[:, i] = dp[:, i - 1]
     wNew, vNew = wv[i, 0], wv[i, 1]
-    dp[wNew:, :] = np.fmax(dp[wNew:, :], vNew + dp[:-wNew, :])
+    #  i-1列では重さがi-wNewのものにはwv[i]の荷物を詰める選択肢がある．
+    #  そこで詰めた結果，最高価値が変わるのかを確認する．
+    dp[wNew:, i] = np.fmax(dp[wNew:, i - 1], vNew + dp[:-wNew, i - 1])
 
 print(dp[w, n - 1])
